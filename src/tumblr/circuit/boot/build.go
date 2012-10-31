30f75d1bb917672348719e112fe710dd9ec866cc
Petar Maymounkov <petarm@gmail.com>
2012-10-31 15:44:44 -0400

    add comments to build config

--- src/tumblr/circuit/boot/build.go
+++ src/tumblr/circuit/boot/build.go
@@ -9,13 +9,14 @@ import (
 
 // BuildConfig holds configuration parameters for the automated circuit app build system
 type BuildConfig struct {
-	Repo             string  // Repo to fetch
-	Pkg              string  // Package to build
-	Host             string  // Build host
-	Tool             string  // Build tool path
-	Jail             string  // Build jail path
-	RebuildGo        bool    // Rebuild Go even if no new version available
-	ShipDir          string  // Local directory where built runtime and libraries will be delivered
+	Repo             string  // User repo to fetch
+	GoPath           string  // User GOPATH relative to user repo; or empty string if user repo meant to be cloned at root of GOPATH
+	Pkg              string  // User side-effect package to include in the circuit runtime build
+	Host             string  // Host where build takes place
+	Tool             string  // Build tool path on build host
+	Jail             string  // Build jail path on build host
+	RebuildGo        bool    // Rebuild Go even if a newer version is not available
+	ShipDir          string  // Local directory where built runtime binary and dynamic libraries will be delivered
 	ZookeeperInclude string  // Path to Zookeeper include files on build host
 	ZookeeperLib     string  // Path to Zookeeper library files on build host
 }
