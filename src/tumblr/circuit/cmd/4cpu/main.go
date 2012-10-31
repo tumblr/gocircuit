b682200480e3179607c90b56e0753ddbf9ce03b0
Petar Maymounkov <petarm@gmail.com>
2012-10-31 16:17:43 -0400

    add midway 4cpu for in-production profiling

---
+++ src/tumblr/circuit/cmd/4cpu/main.go
@@ -0,0 +1,42 @@
+// 4cpu causes a running worker to start CPU profiling for a specified interval, after which it writes the pprof file locally
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	_ "tumblr/circuit/boot"
+	"tumblr/circuit/use/anchorfs"
+	"tumblr/circuit/use/circuit"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		println("Usage:", os.Args[0], "AnchorPath")
+		os.Exit(1)
+	}
+	file, err := anchorfs.OpenFile(os.Args[1])
+	if err != nil {
+		log.Printf("Problem opening (%s)", err)
+		os.Exit(1)
+	}
+	x, err := circuit.TryDial(file.Owner(), "acid")
+	if err != nil {
+		log.Printf("Problem dialing acid service (%s)", err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			log.Printf("Worker disappeared during call (%#v)", p)
+			os.Exit(1)
+		}
+	}()
+
+	retrn := x.Call("RuntimeProfile", "goroutine", 1)
+	if err, ok := retrn[1].(error); ok && err != nil {
+		log.Printf("Problem obtaining runtime profile (%s)", err)
+		os.Exit(1)
+	}
+	fmt.Println(string(retrn[0].([]byte)))
+}
