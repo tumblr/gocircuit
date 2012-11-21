8b0f2cbc692725eaf501ebdabf2c5383ba986ec2
Petar Maymounkov <petarm@gmail.com>
2012-11-21 13:32:51 -0500

    add 4stat: generic tool to print service status

---
+++ src/circuit/cmd/4stat/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	_ "circuit/load"
+	"circuit/use/anchorfs"
+	"circuit/use/circuit"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		println("Usage:", os.Args[0], "AnchorPath ServiceName")
+		os.Exit(1)
+	}
+	file, err := anchorfs.OpenFile(os.Args[1])
+	if err != nil {
+		log.Printf("Problem opening (%s)", err)
+		os.Exit(1)
+	}
+	x, err := circuit.TryDial(file.Owner(), os.Args[2])
+	if err != nil {
+		log.Printf("Problem dialing '%s' service (%s)", os.Args[2], err)
+		os.Exit(1)
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			log.Printf("Worker disappeared during call (%#v)", p)
+			os.Exit(1)
+		}
+	}()
+
+	retrn := x.Call("Stat")
+	fmt.Println(retrn[0].(Stringer).String())
+}
+
+type Stringer interface {
+	String() string
+}
