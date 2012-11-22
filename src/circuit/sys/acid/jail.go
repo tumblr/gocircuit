e0160357123a2c9797041fbec7921e45281466e7
Petar Maymounkov <petarm@gmail.com>
2012-11-22 08:46:20 -0500

    dbg

--- src/circuit/sys/acid/jail.go
+++ src/circuit/sys/acid/jail.go
@@ -9,9 +9,12 @@ import (
 )
 
 func (a *Acid) JailOpen(jailFile string) (circuit.X, error) {
+	println("JailOpen:", jailFile)
 	f, err := os.Open(path.Join(config.Config.Install.JailDir(), jailFile))
 	if err != nil {
+		println("joerr", err.Error())
 		return nil, err
 	}
+	println("aha")
 	return circuit.Ref(file.NewFileServer(f)), nil
 }
