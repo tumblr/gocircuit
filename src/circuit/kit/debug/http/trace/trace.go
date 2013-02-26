b951dd5885f4ce6e39e71404c67055a11e56d8ef
Petar Maymounkov <petarm@gmail.com>
2013-02-26 15:17:56 -0500

    fix

--- src/circuit/kit/debug/http/trace/trace.go
+++ src/circuit/kit/debug/http/trace/trace.go
@@ -7,12 +7,22 @@ import (
 )
 
 func init() {
-	http.HandleFunc("", serveRuntimeProfile)
+	http.HandleFunc("/_pprof", serveRuntimeProfile)
+	http.HandleFunc("/_g", serveGoroutineProfile)
+}
+
+func serveGoroutineProfile(w http.ResponseWriter, r *http.Request) {
+	prof := pprof.Lookup("goroutine")
+	if prof == nil {
+		http.Error(w, "unknown profile name", 400)
+		return
+	}
+	prof.WriteTo(w, 1)
 }
 
 func serveRuntimeProfile(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query().Get("n")
-	debug, err := strconv.Atoi(r.URL.Query().Get("n"))
+	debug, err := strconv.Atoi(r.URL.Query().Get("d"))
 	if err != nil {
 		http.Error(w, "non-integer or missing debug flag", 400)
 		return
