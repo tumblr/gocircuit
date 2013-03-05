c7cfb5584b1d3cc915efd62f29304d2f43b62c08
Petar Maymounkov <petarm@gmail.com>
2013-03-04 20:03:36 -0500

    doc

--- src/circuit/use/circuit/runtime.go
+++ src/circuit/use/circuit/runtime.go
@@ -1,17 +1,26 @@
 package circuit
 
 type runtime interface {
+	// Low-level
 	WorkerAddr() Addr
 	SetBoot(interface{})
+	Kill(Addr) error
+
+	// Spawn mechanism
 	Spawn(Host, []string, Func, ...interface{}) ([]interface{}, Addr, error)
 	Daemonize(func())
-	Kill(Addr) error
+
+	// Cross-services
 	Dial(Addr, string) X
 	DialSelf(string) interface{}
 	TryDial(Addr, string) (X, error)
 	Listen(string, interface{})
+
+	// Persistence of XPerm values
 	Export(...interface{}) interface{}
 	Import(interface{}) ([]interface{}, string, error)
+
+	// Cross-interfaces
 	Ref(interface{}) X
 	PermRef(interface{}) XPerm
 }
